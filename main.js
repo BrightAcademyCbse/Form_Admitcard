cd1b28e357a509e6dd979f06a201f94c509c8701
LokeshShelva <58987457+LokeshShelva@users.noreply.github.com>
2021-03-05 19:06:37 +0530

    hi

---
+++ main.js
@@ -0,0 +1,34 @@
+function handlecheck(checkbox) {
+    if (checkbox.checked) {
+        document.getElementById("submit_button").disabled = false;
+    } else {
+        document.getElementById("submit_button").disabled = true;
+    }
+}
+
+function handleclick(e) {
+    e.preventDefault();
+
+    Sname = document.getElementById("student_name").value
+    Fname = document.getElementById("father_name").value
+
+    document.querySelector("form").reset()
+
+    Sname = Sname.toLowerCase().trim().replace(/\s+/g, '').replace(/\./g, '')
+    Fname = Fname.toLowerCase().trim().replace(/\s+/g, '').replace(/\./g, '')
+
+    console.log(Sname, Fname)
+
+    var xhr = new XMLHttpRequest()
+    var url = "localhost:3000/downloads"
+    xhr.open("POST", url, true)
+    xhr.setRequestHeader("Content-Type", "Application/json")
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var json = JSON.parse(xhr.responseText);
+            console.log(json);
+        }
+    }
+    var data = JSON.stringify({ "Name": Sname, "Father Name": Fname })
+    xhr.send(data)
+}
